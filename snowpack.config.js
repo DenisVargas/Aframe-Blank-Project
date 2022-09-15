5b94a3ba816095ddf58e64e23e31f26517eb4097
Denis Vargas Rivero <denis.vargasrivero@davinci.edu.ar>
2022-09-15 19:18:27 -0300

    Integracion Snowpack & pug

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+    //Copiamos el contenido de las carpetas tal cual.
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+    //Proyecto final.
+  },
+};
